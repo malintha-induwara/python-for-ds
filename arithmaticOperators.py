b5229cf23a15352affc1f1fa90f113a174852a78
Induwara <induwara2k@gmail.com>
2024-11-20 14:39:20 +0530

    feat: add Operators

---
+++ arithmaticOperators.py
@@ -0,0 +1,36 @@
+#Addition
+a = 10
+b = 20
+
+print(a+b)
+
+#Subtraction
+print(a-b)
+
+#Multiplication
+
+print(a*b)
+
+#Division
+print(a/b)
+
+#Modulus
+print(a%b)
+
+#Exponentiation
+print(a**b)
+
+
+#Floor Division
+print(a//b)
+
+#Comparison Operators
+#Equal
+
+print(a==b)
+
+#Not Equal
+print(a!=b)
+
+
+print(9//2)
---
+++ assignmentOperators.py
@@ -0,0 +1,8 @@
+#Assigenment Operators
+x = 5
+
+
+x = x + 5 #x+=5
+
+x = x * 5 #x*=5
+
